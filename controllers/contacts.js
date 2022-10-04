65243ed1ef2f97aa26c5a63ec6319ba0b7a7af29
joaquin solis <sol19008@byui.edu>
2022-10-04 02:12:12 -0300

    adding new routes

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -25,4 +25,83 @@ const getSingleContact = async (req, res, next) => {
   });
 };
 
-module.exports = { getContacts, getSingleContact };
+// use insertOne to add a new contact
+const createContact = async (req, res) => {
+  const contact = {
+    lastName: req.body.lastName,
+    firstName: req.body.firstName,
+    email: req.body.email,
+    favoriteColor: req.body.favoriteColor,
+    birthday: req.body.birthday,
+  };
+  const response = await mongodb
+    .getDb()
+    .db()
+    .collection("contacts")
+    .insertOne(contact);
+
+  if (response.acknowledged) {
+    res.status(201).json(response);
+  } else {
+    res
+      .status(500)
+      .json(
+        response.error || "Some error occurred while creating the contact."
+      );
+  }
+};
+
+// use replaceOne to modify a contact
+const updateContact = async (req, res) => {
+  const userId = new ObjectId(req.params.id);
+  const contact = {
+    lastName: req.body.lastName,
+    firstName: req.body.firstName,
+    email: req.body.email,
+    favoriteColor: req.body.favoriteColor,
+    birthday: req.body.birthday,
+  };
+  const response = await mongodb
+    .getDb()
+    .db()
+    .collection("contacts")
+    .replaceOne({ _id: userId }, contact);
+
+  if (response.modifiedCount > 0) {
+    res.status(204).send();
+  } else {
+    res
+      .status(500)
+      .json(
+        response.error || "Some error occurred while updating the contact."
+      );
+  }
+};
+
+// use deleteOne to delete a contact
+const deleteContact = async (req, res) => {
+  const userId = new ObjectId(req.params.id);
+  const response = await mongodb
+    .getDb()
+    .db()
+    .collection("contacts")
+    .deleteOne({ _id: userId }, true);
+
+  if (response.deletedCount > 0) {
+    res.status(204).send();
+  } else {
+    res
+      .status(500)
+      .json(
+        response.error || "Some error occurred while deleting the contact."
+      );
+  }
+};
+
+module.exports = {
+  getContacts,
+  getSingleContact,
+  createContact,
+  updateContact,
+  deleteContact,
+};
--- routes/contacts.js
+++ routes/contacts.js
@@ -8,5 +8,11 @@ const router = express.Router();
 router.get("/", contactsController.getContacts);
 // GET single contact with "/contacts/:id" URI
 router.get("/:id", contactsController.getSingleContact);
+// POST
+router.post("/", contactsController.createContact);
+// PUT
+router.put("/:id", contactsController.updateContact);
+// DELETE
+router.delete("/:id", contactsController.deleteContact);
 
 module.exports = router;
